0f89eb357fb2028e0230ea589a9ab1713ade1712
jordan-creyelman <creyelmanjo@gmail.com>
2021-10-28 13:41:11 +0200

    finish

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,3 +1,3 @@
 class Specialty < ApplicationRecord
- 
+      has_many   :doctor
 end
--- db/migrate/20211028081655_create_doctors.rb
+++ db/migrate/20211028081655_create_doctors.rb
@@ -4,7 +4,6 @@ class CreateDoctors < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :zip_code
-
       t.timestamps
     end
   end
